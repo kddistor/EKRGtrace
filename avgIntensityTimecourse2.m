84aaf1d73559e196eb04700242482cafa00020af
Kevin Distor <kddistor@ucdavis.edu>
2013-11-14 10:26:04 -0800

    Create avgIntensityTimecourse2.m

---
+++ avgIntensityTimecourse2.m
@@ -0,0 +1,34 @@
+function [data1 data2 data3 data4]=avgIntensityTimecourse2(nameHCube)
+
+ndim=1:size(nameHCube,3);
+timepoints=1:size(nameHCube,1);
+xyPoints=1:size(nameHCube,2);
+threshold=20;
+
+
+for j=timepoints
+    for i=xyPoints
+        for k=ndim
+            imgName=nameHCube{j,i,k};
+            currentImage = imread(imgName);
+            background1=imopen(currentImage,strel('disk',50));
+            currentImage=currentImage-background1;
+            imgBlock(:,:,k)=currentImage;
+        end
+        
+        currentCFPimage=imgBlock(:,:,1);
+        currentYFPimage=imgBlock(:,:,2);
+        ratioImage=imgBlock(:,:,1)./imgBlock(:,:,2);
+        tImage=imgBlock(:,:,1)>threshold;
+        
+        currentCFPpix=currentCFPimage(tImage);
+        currentYFPpix=currentYFPimage(tImage);
+        currentRatiopix=ratioImage(tImage);
+        
+        data1(i,j)=mean(currentCFPpix);
+        data2(i,j)=mean(currentYFPpix);
+        data3(i,j)=mean(currentRatiopix);
+        data4(i,j)=mean(currentYFPpix)/mean(currentCFPpix);
+        
+    end
+end
